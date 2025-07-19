0a05367a27b3e9ba3c7d80613d527cd9558dc77f
RM <mus_rob@o2.pl>
2025-07-19 18:48:03 +0200

    V.2.4

--- test.asm
+++ test.asm
@@ -1,25 +1,25 @@
-			;Drugi program V.2.3
+			;Drugi program V.2.4
 
 		org $2000
 
-        lda #'W'
-        sta $9C40
-        lda #'y'
-        sta $9C41
-        lda #'n'
-        sta $9C42
-        lda #'i'
-        sta $9C43
-        lda #'k'
-        sta $9C44
-        lda #':'
-        sta $9C45
+        lda #55
+        sta 40010
+        lda #57
+        sta 40011
+        lda #46
+        sta 40012
+        lda #41
+        sta 40013
+        lda #43
+        sta 40014
+        lda #26
+        sta 40015
 
         ; Dodawanie 5 + 3
         lda #5
         clc
         adc #3
         adc #'0'      ; Zamiana liczby na kod ASCII
-        sta $9C46     ; Wstawienie cyfry po napisie "Wynik:"
+        sta 40016    ; Wstawienie cyfry po napisie "Wynik:"
 
         jmp $a000     ; skok do Atari Basic
